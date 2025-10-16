530fca3dd12421300058be90899a9215bfe0c91c
Lorraine Bichara Assad <lorbichara@gmail.com>
2025-10-16 13:06:13 -0400

    Restructure folder organization

--- src/rag/docker-shell.sh
+++ src/rag/docker-shell.sh
@@ -7,7 +7,7 @@ set -e
 export BASE_DIR=$(pwd)
 export PERSISTENT_DIR=$(pwd)/../persistent-folder/
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCP_PROJECT="ac215-bhargav" # CHANGE TO YOUR PROJECT ID
+export GCP_PROJECT="ac215-f1penaltytool" # CHANGE TO YOUR PROJECT ID
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/llm-service-account.json"
 export IMAGE_NAME="ac215-rag-cli"
 
