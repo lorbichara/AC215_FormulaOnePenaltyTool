d034d441acd922cde16338be168ee25363a10bae
Bhargav Kosaraju <kosaraju_b@yahoo.com>
2025-11-25 01:10:28 -0800

    Added API backend capabilities to RAG engine

--- docker-shell.sh
+++ docker-shell.sh
@@ -16,17 +16,4 @@ docker build -t $IMAGE_NAME -f Dockerfile .
 
 echo "Running container"
 
-#docker run --rm --name $IMAGE_NAME -ti \
-#--cap-add SYS_ADMIN \
-#--device /dev/fuse \
-#-v "$BASE_DIR":/app \
-#-v "$SECRETS_DIR":/secrets \
-#-v "chromadb":/chroma/chroma \
-#-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
-#-e GCP_BUCKET=$GCP_BUCKET \
-#-e GCP_PROJECT=$GCP_PROJECT \
-#-e GCP_ZONE=$GCP_ZONE \
-#-p 8000:8000 \
-#$IMAGE_NAME
-
 docker-compose run --rm --service-ports $IMAGE_NAME
---
+++ src/rag/main.py
@@ -0,0 +1,60 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from starlette.middleware.cors import CORSMiddleware
+
+import rag
+
+# Setup FastAPI app
+app = FastAPI(title="API Server", description="API Server", version="v1")
+
+# Enable CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=False,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
+@app.get("/")
+async def get_index():
+    return {"message": "Welcome to Formula One Penalty Analysis Tool"}
+
+@app.get("/chunk/")
+async def create_chunks():
+    ret_str, ret_val = rag.create_chunks()
+    html_content = f"""
+        {ret_str}
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/embed/")
+async def create_embeddings():
+    ret_str = rag.create_embeddings()
+    html_content = f"""
+        {ret_str}
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/store/")
+async def store_embeddings():
+    ret_str = rag.store_embeddings()
+    html_content = f"""
+        {ret_str}
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/query/")
+async def query_llm(
+        prompt: str):
+    ret_str = rag.query(prompt)
+    html_content = f"""
+        {ret_str}
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, log_level="info")
