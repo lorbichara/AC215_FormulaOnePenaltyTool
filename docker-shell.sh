225215ed58464135b8106ae5b8096ae322b50bff
Bhargav Kosaraju <kosaraju_b@yahoo.com>
2025-11-25 17:12:43 -0800

    Fixed formatting issues and reduced clutter in docker files

--- docker-shell.sh
+++ docker-shell.sh
@@ -6,11 +6,6 @@ export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/secrets/
 export IMAGE_NAME="ac215-rag"
 
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/ac215-f1penaltytool.json"
-export GCP_BUCKET="f1penaltydocs"
-export GCP_PROJECT="ac215-f1penaltytool"
-export GCP_ZONE="us-central1-a"
-
 echo "Building image"
 docker build -t $IMAGE_NAME -f Dockerfile .
 
