18b0699b98e6a24aa1b33e7cf42e749db7c7449e
Bhargav Kosaraju <kosaraju_b@yahoo.com>
2025-11-27 10:14:50 -0800

    Enabled CI/CD mechanism

--- docker-shell.sh
+++ docker-shell.sh
@@ -3,12 +3,17 @@
 set -e
 
 export BASE_DIR=$(pwd)
-export SECRETS_DIR=$(pwd)/secrets/
-export IMAGE_NAME="ac215-rag"
+export IMAGE_NAME="rag-img"
 
 echo "Building image"
 docker build -t $IMAGE_NAME -f Dockerfile .
 
 echo "Running container"
+#docker compose run --rm --service-ports rag
+#  NOTE: docker-compose adds some suffix to the container name
+#
 
-docker-compose run --rm --service-ports $IMAGE_NAME
+#docker compose run --rm --service-ports --name rag rag
+
+#docker compose up
+docker compose up -d rag
